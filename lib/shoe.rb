101ba0675080270f3b3b41dc8e08b7918081cbf5
pledgedavis <pledgedavis14@gmail.com>
2020-01-07 03:03:42 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,12 @@
 # Make your shoe class here!
 class Shoe 
    attr_accessor :brand, :color, :size, :material, :condition
-    def cobble
   def initialize(brand)
     @brand = (brand)
   end
-  attr_accessor :brand, :color, :size, :material, :condition
     def cobble
       puts "Your shoe is as good as new!" 
-   
+   @condition ="new"
   
 end
 
